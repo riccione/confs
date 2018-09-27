3016b0fab10d18328d1e672000e56aeb7798d473
riccione <riccione@protonmail.com>
2018-09-27 02:52:38 -0400

    Updated jmeter.sh: - new version of the jmeter - added check if dir (bin) exists

--- jmeter.sh
+++ jmeter.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-jmeter_url="http://us.mirrors.quenda.co/apache//jmeter/binaries/apache-jmeter-4.0.tgz"
+bin_dir=~/bin
+jmeter_url="http://mirrors.sorengard.com/apache//jmeter/binaries/apache-jmeter-5.0.tgz"
+sha_url="https://www.apache.org/dist/jmeter/binaries/apache-jmeter-5.0.tgz.sha512"
+# todo: add verify the integrity
+# exit 1
+
+# https://jmeter-plugins.org/wiki/PluginsManager/#Installation-and-Usage
 jmeter_plugins_manager="http://search.maven.org/remotecontent?filepath=kg/apc/jmeter-plugins-manager/1.3/jmeter-plugins-manager-1.3.jar"
 
 echo $jmeter_url
@@ -15,3 +21,10 @@ tar -xvzf $filename
 filename=${jmeter_plugins_manager##*/}
 wget --content-disposition $jmeter_plugins_manager
 mv $filename $filename_no_ext/lib/ext
+
+# move folder to the ~/bin/  
+if [ ! -d "$bin_dir" ]; then
+	mkdir $bin_dir
+fi
+
+mv $filename_no_ext/ $bin_dir/
