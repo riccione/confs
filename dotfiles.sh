f701983e304fcbd31421e55c4ec8f6c35e113ee2
Zero <zero@zero-ms7c56.ignes-zero.com>
2024-08-28 22:43:45 -0400

    Add color theme PaperColor

--- dotfiles.sh
+++ dotfiles.sh
@@ -6,7 +6,11 @@ GITHUB_USERNAME="riccione"
 # github
 sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply $GITHUB_USERNAME
 
-
 # install Plug for vim
+# https://github.com/junegunn/vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# install color theme
+# https://github.com/NLKNguyen/papercolor-theme
+git clone https://github.com/NLKNguyen/papercolor-theme.git ~/.vim/pack/colors/start/papercolor-theme
